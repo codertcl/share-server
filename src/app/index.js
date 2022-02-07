7bbfaa5e6dcfa5efa74f3f45c2a595798bd87b97
wuxinabc <45070627+wuxinabc@users.noreply.github.com>
2022-02-07 17:32:47 +0800

    feat:koa2-cors包允许跨域

--- src/app/index.js
+++ src/app/index.js
@@ -3,7 +3,8 @@ const bodyParser = require('koa-bodyparser')
 const errorHandler = require('./error-handle')
 const useRoutes = require('../router/index')
 const app = new Koa()
-
+var cors = require('koa2-cors');
+app.use(cors());
 app.use(bodyParser());
 //注册使用路由 useRoutes函数中this绑定到了app
 app.useRoutes = useRoutes;
--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,16 +1,27 @@
 const jwt = require('jsonwebtoken')
-const {PRIVATE_KEY} = require("../app/config");
+const {
+    PRIVATE_KEY
+} = require("../app/config");
 
 class authController {
     async login(ctx, next) {
         // 获取用户请求传递的参数
-        const {id, name} = ctx.user
-        const token = jwt.sign({id, name}, PRIVATE_KEY, {
+        const {
+            id,
+            name
+        } = ctx.user
+        const token = jwt.sign({
+            id,
+            name
+        }, PRIVATE_KEY, {
             expiresIn: 60 * 60 * 24,
             algorithm: 'RS256'
         })
+        ctx.status = 200
         ctx.body = {
-            id, name, token
+            id,
+            name,
+            token
         }
     }
 
