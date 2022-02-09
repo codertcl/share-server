cee04c0191702bfc3086ac58a8b7198e054e27ab
wuxinabc <45070627+wuxinabc@users.noreply.github.com>
2022-02-09 11:28:29 +0800

    fix:优化登录和注册接口的返回信息

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,24 +1,32 @@
-const fs=require('fs')
+const fs = require('fs')
 const userService = require('../service/user.service')
 const fileService = require('../service/file.service')
-const {AVATAR_PATH}=require('../constants/file-path')
+const {
+    AVATAR_PATH
+} = require('../constants/file-path')
 class userController {
     //1:创建用户
     async create(ctx, next) {
         // 获取用户请求传递的参数
         const user = ctx.request.body
         // 获取SQL语句执行结果,设置到ctx.body中
+        await userService.create(user)
         // 返回数据
-        ctx.body = await userService.create(user)
+        ctx.body = {
+            status: 200,
+            message: '注册成功,赶快登录吧'
+        }
     }
 
 
     //2:获取头像信息
     async avatarInfo(ctx, next) {
         // 获取用户请求传递的参数
-        const {userId} = ctx.params
+        const {
+            userId
+        } = ctx.params
         const avatarInfo = await fileService.getAvatarByUserId(userId)
-        ctx.response.set('content-type',avatarInfo.mimeType)
+        ctx.response.set('content-type', avatarInfo.mimeType)
         ctx.body = fs.createReadStream(`${AVATAR_PATH}/${avatarInfo.filename}`)
     }
 }
--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -4,12 +4,15 @@ const md5password = require('../utils/password-handle')
 //验证用户信息
 const verifyUser = async (ctx, next) => {
     //1:获取用户信息
-    const {name, password} = ctx.request.body
+    const {
+        name,
+        password
+    } = ctx.request.body
 
     //2:用户信息不能为空  空字符串==null false  !''为TRUE
     if (!name || !password) {
         const error = new Error(errorType.NAME_OR_PASSWORD_IS_NOT_REQUIRED)
-        return ctx.app.emit('error', error, ctx)//第一个参数为事件名称 后面为参数
+        return ctx.app.emit('error', error, ctx) //第一个参数为事件名称 后面为参数
     }
     //3:判断本次注册用户是否已经存在
     const result = await service.getUserByName(name);
@@ -24,9 +27,11 @@ const verifyUser = async (ctx, next) => {
 
 //加密密码
 const handlePassword = async (ctx, next) => {
-    let {password} = ctx.request.body;
+    let {
+        password
+    } = ctx.request.body;
     ctx.request.body.password = md5password(password);
-    await next();//调用下一个中间件或者函数
+    await next(); //调用下一个中间件或者函数
 }
 module.exports = {
     verifyUser,
