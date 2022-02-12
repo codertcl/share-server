c72ac399d872bac0aff03d98d02bde8ae2c9c5f5
wuxinabc <45070627+wuxinabc@users.noreply.github.com>
2022-02-12 18:36:40 +0800

    feat:发布动态时接口返回动态id

--- src/controller/moment.controller.js
+++ src/controller/moment.controller.js
@@ -1,6 +1,8 @@
 const momentService = require('../service/moment.service')
 const fileService = require('../service/file.service')
-const {PICTURE_PATH} = require("../constants/file-path");
+const {
+    PICTURE_PATH
+} = require("../constants/file-path");
 const fs = require('fs')
 
 class momentController {
@@ -8,11 +10,15 @@ class momentController {
         //1:获取user_id content
         const userId = ctx.user.id;
         const content = ctx.request.body.content;
-        console.log(userId, content)
 
+        let res = await momentService.create(userId, content)
         //2:将数据插入到数据库
-        // 获取SQL语句执行结果,设置到ctx.body中
-        ctx.body = await momentService.create(userId, content)
+        // 获取该动态在表中的id,便于和上传的图片关联并设置到ctx.body中
+        ctx.body = {
+            status: 200,
+            message: '发布成功',
+            momentId: res[0].insertId
+        }
     }
 
 
@@ -30,7 +36,10 @@ class momentController {
     ////分页查询查询多条动态
     async list(ctx, next) {
         //1:获取查询参数
-        const {offset, size} = ctx.query;
+        const {
+            offset,
+            size
+        } = ctx.query;
         //2:查询列表
         const res = await momentService.getMomentList(offset, size)
         ctx.body = res
@@ -39,8 +48,12 @@ class momentController {
     ////更新动态信息
     async update(ctx, next) {
         // //1:获取动态id momentId
-        const {momentId} = ctx.params;
-        const {content} = ctx.request.body;
+        const {
+            momentId
+        } = ctx.params;
+        const {
+            content
+        } = ctx.request.body;
 
         // //2:查根据momentId更新动态信息
         const res = await momentService.update(momentId, content)
@@ -50,7 +63,9 @@ class momentController {
     ////删除动态信息
     async remove(ctx, next) {
         // //1:获取动态id momentId
-        const {momentId} = ctx.params;
+        const {
+            momentId
+        } = ctx.params;
 
         // //2:查根据momentId删除动态信息
         ctx.body = await momentService.remove(momentId)
@@ -59,8 +74,12 @@ class momentController {
     //为动态添加标签
     async addLabels(ctx, next) {
         // //1:获取动态 momentId
-        const {momentId} = ctx.params;
-        const {labels} = ctx;
+        const {
+            momentId
+        } = ctx.params;
+        const {
+            labels
+        } = ctx;
         console.log(momentId, labels)
 
         //2：添加所有标签
@@ -77,13 +96,18 @@ class momentController {
     //配置浏览器请求访问动态图片的接口
     async fileInfo(ctx, next) {
         // 获取用户请求传递的参数
-        let {filename} = ctx.params;
+        let {
+            filename
+        } = ctx.params;
         const fileInfo = await fileService.getFileByFilename(filename)
         //通过params参数设置三种格式图片的展示
-        const {type} = ctx.query
+        const {
+            type
+        } = ctx.query
         const types = ['small', 'middle', 'large']
         if (types.indexOf(type) !== -1) {
-            let name = filename.split('.')[0], extraname = filename.split('.')[1]
+            let name = filename.split('.')[0],
+                extraname = filename.split('.')[1]
             filename = name + '-' + type + '.' + extraname
         }
         ctx.response.set('content-type', fileInfo.mimeType)
